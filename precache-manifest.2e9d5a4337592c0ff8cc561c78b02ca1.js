0ce364a56987e096788454238f6a2f26efaa0e0d
henrytran721 <htran@ucsd.edu>
2019-08-16 21:47:05 -0700

    Updates

---
+++ precache-manifest.2e9d5a4337592c0ff8cc561c78b02ca1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e932b0bf446d3d1b947b2fb699e54d",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "39eb56f028f53ef1f454",
+    "url": "/personal-website/static/css/main.aa48d831.chunk.css"
+  },
+  {
+    "revision": "db6968ba635e5306c17b",
+    "url": "/personal-website/static/js/2.2ff469e9.chunk.js"
+  },
+  {
+    "revision": "39eb56f028f53ef1f454",
+    "url": "/personal-website/static/js/main.1f9c6cbe.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "85d4420cd351a38ac0383bd5f64535a5",
+    "url": "/personal-website/static/media/Fontania.85d4420c.ttf"
+  },
+  {
+    "revision": "c224ee64f20e5129386a6d58c36f7e68",
+    "url": "/personal-website/static/media/LemonMilklight.c224ee64.otf"
+  },
+  {
+    "revision": "132cf8fe10cfacd835f8de14b2e0650e",
+    "url": "/personal-website/static/media/input.132cf8fe.svg"
+  },
+  {
+    "revision": "077e2e1dfbf6e13a0d42811d38c8caf3",
+    "url": "/personal-website/static/media/map.077e2e1d.svg"
+  },
+  {
+    "revision": "26c9378b1452aae49990db1b7c800a9b",
+    "url": "/personal-website/static/media/needsAndInsights.26c9378b.svg"
+  },
+  {
+    "revision": "787339b8a2be985dc5cd5460e7af38fb",
+    "url": "/personal-website/static/media/nimbusFinal.787339b8.svg"
+  },
+  {
+    "revision": "831bdc3044cf468d72a6c7d3b1f9a158",
+    "url": "/personal-website/static/media/post.831bdc30.svg"
+  },
+  {
+    "revision": "89884a988377e6fcb52746323f0551dc",
+    "url": "/personal-website/static/media/prishi.89884a98.png"
+  },
+  {
+    "revision": "5d5867c269d988dc6c20950690bc099a",
+    "url": "/personal-website/static/media/profile.5d5867c2.png"
+  },
+  {
+    "revision": "2a274bc6361b6cc433ed2d6959e90f0b",
+    "url": "/personal-website/static/media/prototypeOne.2a274bc6.svg"
+  },
+  {
+    "revision": "fd7e9808e9c0f0428ea97e32c11712be",
+    "url": "/personal-website/static/media/prototypeOneB.fd7e9808.svg"
+  },
+  {
+    "revision": "ef44f685637f7b0c43671a4c322ed5cd",
+    "url": "/personal-website/static/media/prototypeTwo.ef44f685.svg"
+  },
+  {
+    "revision": "59de115a317510d876e699801e61bb5c",
+    "url": "/personal-website/static/media/prototypeTwoB.59de115a.svg"
+  },
+  {
+    "revision": "f6b9f3856a39d878d7e9e797df74d000",
+    "url": "/personal-website/static/media/rishichildren.f6b9f385.svg"
+  },
+  {
+    "revision": "6437f15801048f19f8456d592be37920",
+    "url": "/personal-website/static/media/userOne.6437f158.svg"
+  },
+  {
+    "revision": "f90d281ae0169250850ffbdc6dc54148",
+    "url": "/personal-website/static/media/userThree.f90d281a.svg"
+  },
+  {
+    "revision": "e9d234478959a684d509882170cd91bb",
+    "url": "/personal-website/static/media/userTwo.e9d23447.svg"
+  },
+  {
+    "revision": "5d54091f54023be17bee3ec1a702b286",
+    "url": "/personal-website/static/media/wireframe.5d54091f.svg"
+  }
+]);
