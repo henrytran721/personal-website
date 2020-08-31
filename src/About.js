5abe5729d90242e0411255a4bba77f7029041ae7
henrytran721 <htran@ucsd.edu>
2020-08-31 13:15:06 -0700

    changed about me section

--- src/About.js
+++ src/About.js
@@ -7,40 +7,43 @@ export default class About extends Component {
     render() {
         return(
             <div id='about'>
-                <div className='textContainer'>
-                    <div className='aboutText'><p>About Me</p></div>
-                </div>
-                <div className='gradientFlex'>
-                    <div className="gradient-border" id="box">
-                        <div className='firstRowGradient'>
-                            <img className='profile' src={profile} />
-                            <div className='firstRowText'>
-                                <p className='firstHeader'>Hi I'm Henry!</p>
-                                <p className='firstInfo'>I’m a recent graduate from ucsd who majored in cognitive science with a specialization in human computer interaction. 
-                                   I am Currently located in Los Angeles, CA. When i’m not coding, i like to spend my time photographing and lifting weights.</p>
-                            </div>
-                        </div>
-                        <div className='secondRowGradient'>
-                            <p className='mySkills'>My Skills</p>
-                            <div className='lists'>
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>Bootstrap</li>
-                                <li>Sass</li>
-                                <li>UI/UX Design</li>
-                            </ul>
-                            <ul>
-                                <li>Sketch</li>
-                                <li>Git</li>
-                                <li>JavaScript</li>
-                                <li>React.js</li>
-                                <li>Webpack</li>
-                            </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <img src='https://i.imgur.com/5ax6Aj1.jpg' />
+                <svg width="642px" height="595px" viewBox="0 0 642 595" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <title>A4</title>
+                <desc>Created with Sketch.</desc>
+                <defs>
+                <linearGradient x1="79.6914398%" y1="36.6248725%" x2="48.5199671%" y2="92.4643469%" id="linearGradient-1">
+                <stop stop-color="#007AB7" offset="0%"></stop>
+                <stop stop-color="#00FF8D" offset="99.9023438%"></stop>
+                </linearGradient>
+                </defs>
+                <g id="A4" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <rect fill="#FFFFFF" x="0" y="0" width="642" height="595"></rect>
+                <rect id="Rectangle" stroke="#979797" fill="url(#linearGradient-1)" x="0" y="0" width="642" height="595"></rect>
+                <path d="M-591,278.856569 C-190.903887,329.714477 79.3987554,329.714477 219.907926,278.856569 C290.11532,253.444697 356.475684,202.569746 430.671057,240.713156 C480.134639,266.142096 550.57762,278.856567 642,278.856569" id="Line" stroke-opacity="0.397836538" stroke="#000000" stroke-width="4" stroke-linecap="square"></path>
+                <path d="M-590,331.856569 C-189.903887,382.714477 80.3987554,382.714477 220.907926,331.856569 C291.11532,306.444697 357.475684,255.569746 431.671057,293.713156 C481.134639,319.142096 551.57762,331.856567 643,331.856569" id="Line-Copy" stroke-opacity="0.379397946" stroke="#000000" stroke-width="4" stroke-linecap="square"></path>
+                <path d="M-592,384.856569 C-191.903887,435.714477 78.3987554,435.714477 218.907926,384.856569 C289.11532,359.444697 355.475684,308.569746 429.671057,346.713156 C479.134639,372.142096 549.57762,384.856567 641,384.856569" id="Line-Copy-2" stroke-opacity="0.144558566" stroke="#000000" stroke-width="4" stroke-linecap="square"></path>
+                <text id="ABOUT-ME" font-family="FontaniaRegular, Fontania" font-size="50" font-weight="normal" fill="#FFFFFF">
+                <tspan x="226" y="79">ABOUT ME</tspan>
+                </text>
+                <text id="Hey-I’m-Henry.-I’m-a" font-family="Avenir-Medium, Avenir" font-size="20" font-weight="400" fill="#FFFFFF">
+                <tspan x="26" y="133">Hey I’m Henry. I’m a Full Stack Developer originally from Los</tspan>
+                <tspan x="26" y="160">Angeles. I like to build web pages that take user interactions</tspan>
+                <tspan x="26" y="187">and design into account to fulfill the needs of the user</tspan>
+                <tspan x="26" y="214">experience. I am always curious about web architecture and</tspan>
+                <tspan x="26" y="241">am always looking for the next thing to add to my skillset.</tspan>
+                </text>
+                <text id="Some-frameworks-and" font-family="Avenir-Medium, Avenir" font-size="20" font-weight="400" fill="#FFFFFF">
+                <tspan x="25" y="324">Some frameworks and languages I’ve worked with are HTML,</tspan>
+                <tspan x="25" y="351">CSS, Sass, JavaScript, MongoDB, Express.js, Node.js, React.js,</tspan>
+                <tspan x="25" y="378">Wordpress Environment, PHP, and many more! </tspan>
+                </text>
+                <text id="Outside-of-coding,-s" font-family="Avenir-Medium, Avenir" font-size="20" font-weight="400" fill="#FFFFFF">
+                <tspan x="19" y="466">Outside of coding, some of my hobbies include working out,</tspan>
+                <tspan x="19" y="493">playing video games (Valorant and Fortnite), and exploring nature.</tspan>
+                </text>
+                </g>
+                </svg>
             </div>
         )
     }
